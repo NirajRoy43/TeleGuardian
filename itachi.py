6f09ddc246e6a6b371e92411da9453bfedb06b1e
Niraj Kumar <149550225+NirajRoy43@users.noreply.github.com>
2024-09-08 09:32:53 +0530

    Update itachi.py

--- itachi.py
+++ itachi.py
@@ -12,7 +12,7 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 with open('text.json', 'r', encoding='utf-8') as f:
-    messages = json.load(f)
+    text = json.load(f)
 
 # Your Telegram API credentials from environment variables
 api_id = os.getenv('API_ID')
